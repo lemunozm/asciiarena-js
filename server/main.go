f3438a5a1e563cfe7ce41371cd0f8731b7412c9b
Luis Enrique Muñoz Martín <lemunozm@gmail.com>
2017-12-28 07:55:44 +0100

    Test os.Args

--- server/main.go
+++ server/main.go
@@ -2,48 +2,51 @@ package main
 
 import cm "../common"
 import "fmt"
+import "os"
 
 func main() {
-    argumentList := cm.NewArgumentList()
+	argumentList := cm.NewArgumentList()
 
-    argumentList.Add(&cm.Argument{
-        Name:                      "help",
-        Description:               "Show help",
-        Ids:                       []string{"--help", "-h"},
-        DisableRequiredArguments:  true,
-    })
+	argumentList.Add(&cm.Argument{
+		Name:        "help",
+		Description: "Show help",
+		Ids:         []string{"--help", "-h"},
+		DisableRequiredArguments: true,
+	})
 
-    argumentList.Add(&cm.Argument{
-        Name:                      "port",
-        Description:               "Set the port of the server",
-        Ids:                       []string{"--port"},
-        ValuesName:                []string{"number"}, 
-        DefaultValues:             []string{"3000"},
-    })
+	argumentList.Add(&cm.Argument{
+		Name:          "port",
+		Description:   "Set the port of the server",
+		Ids:           []string{"--port"},
+		ValuesName:    []string{"number"},
+		DefaultValues: []string{"3000"},
+	})
 
-    argumentList.Add(&cm.Argument{
-        Name:                      "players",
-        Description:               "Set the number of players for the match",
-        Ids:                       []string{"--players", "-p"},
-        ValuesName:                []string{"number"}, 
-        Required:                  true,
-    })
+	argumentList.Add(&cm.Argument{
+		Name:        "players",
+		Description: "Set the number of players for the match",
+		Ids:         []string{"--players", "-p"},
+		ValuesName:  []string{"number"},
+		Required:    true,
+	})
 
-    argumentList.Add(&cm.Argument{
-        Name:                      "waitMatch",
-        Description:               "Waiting to initialize the match once all players are connected",
-        Ids:                       []string{"--wait-match"},
-        ValuesName:                []string{"number"}, 
-        DefaultValues:             []string{"3000"},
-    })
+	argumentList.Add(&cm.Argument{
+		Name:          "waitMatch",
+		Description:   "Waiting to initialize the match once all players are connected",
+		Ids:           []string{"--wait-match"},
+		ValuesName:    []string{"number"},
+		DefaultValues: []string{"3000"},
+	})
 
-    argumentList.Add(&cm.Argument{
-        Name:                      "mapSeed",
-        Description:               "Seed for generate a map. 0 means that the maps will be generated randomly",
-        Ids:                       []string{"--map-seed", "-ms"},
-        ValuesName:                []string{"number"}, 
-        DefaultValues:             []string{"0"},
-    })
+	argumentList.Add(&cm.Argument{
+		Name:          "mapSeed",
+		Description:   "Seed for generate a map. 0 means that the maps will be generated randomly",
+		Ids:           []string{"--map-seed", "-ms"},
+		ValuesName:    []string{"number"},
+		DefaultValues: []string{"0"},
+	})
 
-    fmt.Println(argumentList)
+	argumentList.Parse(os.Args)
+
+	fmt.Println(argumentList)
 }
