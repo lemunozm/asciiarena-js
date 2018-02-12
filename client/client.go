21309228e772c18de32bf9121f77e6a9a41eccf8
Luis Enrique Muñoz Martín <lemunozm@gmail.com>
2018-02-12 08:04:45 +0100

    Added connection.go

--- client/client.go
+++ client/client.go
@@ -1,16 +1,44 @@
 package client
 
 import "fmt"
+import "net"
+import "github.com/lemunozm/ASCIIArena/common"
 
 type Client struct {
-	host string
-	port string
+	message *common.Message
 }
 
 func NewClient() Client {
-	return Client{}
+	message := &common.NewMessage()
+
+	return Client{message}
 }
 
-func (client *Client) Run(host string, port uint) {
+func (c *Client) Run(host string, port uint) {
 	fmt.Printf("Connect to server on: %s:%d\n", host, port)
+
+	localAddress, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
+	remoteAddress, err := net.ResolveUDPAddr("udp", host+":"+port)
+	connection, err := net.DialUDP("udp", localAddress, remoteAddress)
+	defer connection.Close()
+
+	versionData := VersionData{"0.0.0"}
+	c.SendVersionData(&versionData)
+
+	for {
+		length, err := connection.Read(message.Buffer())
+		data := message.Deserialize()
+		networkDataCallback.give(data, remoteAddress)
+	}
+}
+
+func (c *Client) VersionCheckedDataReceived(data interface{}) {
+	if versionCheckedData, ok := data.(common.VersionCheckedData); ok {
+		fmt.Println("Received VersionCheckedData:", versionCheckedData)
+	}
+}
+
+func (c *Client) SendVersionData(data *common.VersionData) {
+	message.Serialize(data)
+	connection.Write(message.Buffer())
 }
--- server/server.go
+++ server/server.go
@@ -2,6 +2,7 @@ package server
 
 import "bytes"
 import "fmt"
+import "net"
 import "github.com/lemunozm/ASCIIArena/common"
 
 type Server struct {
@@ -9,31 +10,44 @@ type Server struct {
 }
 
 func NewServer() Server {
+	serializer := &common.NewSerializer()
+	deserializer := &common.NewDeserializer()
+	deserializer.RegisterCallback(common.VersionData{}, s.VersionDataReceived)
+
 	return Server{}
 }
 
 func (s *Server) Run(port uint) {
 	fmt.Printf("Server listening on: %d\n", port)
 
+	localAddress, err := net.ResolveUDPAddr("udp", ":"+port)
+	connection, err := net.ListenUDP("udp", localAddress)
+	defer connection.Close()
+
+	for {
+		length, remoteAddress, err := connection.ReadFromUDP(deserializer.Buffer())
+		deserializer.Deserialize()
+	}
+
 	var network bytes.Buffer
-	
+
 	message := common.NewMessage(&network)
 
 	versionData := common.VersionData{"0.0.0"}
 	message.Write(versionData)
 
-    message.RegisterCallback(common.VersionData{}, s.VersionDataReceived)
+	message.RegisterCallback(common.VersionData{}, s.VersionDataReceived)
 
-    message.Read()
+	message.Read()
 }
 
-func (s* Server) VersionDataReceived(data interface{}) {
+func (s *Server) VersionDataReceived(data interface{}) {
 	if versionData, ok := data.(common.VersionData); ok {
-        fmt.Println("Post serializacion A:", versionData)
-    }
-    //add address to the arguments??? Implicaria que el mensaje supiese del network. Creo que mejor no, (diferencias entre cliente y servidor)
+		fmt.Println("Received VersionData:", versionData)
+	}
+	//add address to the arguments??? Implicaria que el mensaje supiese del network. Creo que mejor no, (diferencias entre cliente y servidor)
 }
 
-func (s* Server) SendVersionCheckedData(data *common.VersionCheckedData) { 
+func (s *Server) SendVersionCheckedData(data *common.VersionCheckedData) {
 	//add address to the arguments
 }
