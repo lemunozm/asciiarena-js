fd9bb7b756a913ad442537b803ec51e330dc04f4
Luis Enrique Muñoz Martín <lemunozm@gmail.com>
2019-02-01 23:50:05 +0100

    Modified keyboard controller to at least gets a key event if it occurs in the last frame.

--- asciiarena/client/keyboard.py
+++ asciiarena/client/keyboard.py
@@ -8,17 +8,20 @@ class Key(enum.Enum):
     D = enum.auto()
     S = enum.auto()
     W = enum.auto()
-    UNKNOWN = enum.auto()
 
+class TypeEvent(enum.Enum):
+    PRESSED = enum.auto()
+    RELEASED = enum.auto()
 
 class Keyboard:
     def __init__(self):
-        self._local_key_dict = {}
-        self._global_key_dict = {}
-        self._global_key_to_remove_list = []
         self._listener = pynput.keyboard.Listener(on_press = self._on_press, on_release = self._on_release)
         self._listener.start()
         self._mutex = threading.Lock()
+        self._global_key_pressed_list = []
+        self._global_key_released_list = []
+        self._global_key_dict = {}
+        self._local_key_dict = {}
 
 
     def close(self):
@@ -29,46 +32,61 @@ class Keyboard:
         return self._local_key_dict.get(key, False)
 
 
-    def update_key_events(self, key_event_list):
-        for key_event in key_event_list:
-            key = Keyboard._key_event_to_key(key_event)
-            self._local_key_dict[key] = True
+    def update_key_events(self, local_key_list):
+        # Update local dict
+        for local_key in local_key_list:
+            key = Keyboard._local_key_to_common_key(local_key)
+            if key:
+                self._local_key_dict[key] = True
 
-        local_key_to_remove_list = []
         with self._mutex:
+            # Update global dict with pressed events
+            for key in self._global_key_pressed_list:
+                self._global_key_dict[key] = True
+
+            # Update local with global information
+            local_key_to_remove_list = []
             for key in self._local_key_dict.keys():
                 if not self._global_key_dict.get(key, False):
                     local_key_to_remove_list.append(key)
 
-        for key in local_key_to_remove_list:
-            self._local_key_dict.pop(key)
+            for key in local_key_to_remove_list:
+                self._local_key_dict.pop(key)
+
+            # Update global dict with released events
+            for key in self._global_key_released_list:
+                self._global_key_dict[key] = False
+
+            self._global_key_pressed_list = []
+            self._global_key_released_list = []
+
+    def _on_press(self, global_key):
+        key = Keyboard._global_key_to_common_key(global_key)
+        if key:
+            with self._mutex:
+                self._global_key_pressed_list.append(key)
 
 
-    def _on_press(self, key_code):
-        with self._mutex:
-            key = Keyboard._key_code_to_key(key_code)
-            self._global_key_dict[key] = True
-
-
-    def _on_release(self, key_code):
-        with self._mutex:
-            key = Keyboard._key_code_to_key(key_code)
-            self._global_key_dict.pop(key)
+    def _on_release(self, global_key):
+        key = Keyboard._global_key_to_common_key(global_key)
+        if key:
+            with self._mutex:
+                self._global_key_released_list.append(key)
 
 
     @staticmethod
-    def _key_event_to_key(key_event):
+    def _local_key_to_common_key(local_key):
         switcher = {
             97:  Key.A,
             100: Key.D,
             115: Key.S,
             119: Key.W,
         }
-        return switcher.get(key_event, Key.UNKNOWN)
+        return switcher.get(local_key, None)
 
 
     @staticmethod
-    def _key_code_to_key(key_code):
+    def _global_key_to_common_key(global_key):
         try:
             switcher_chars = {
                 'a': Key.A,
@@ -76,8 +94,8 @@ class Keyboard:
                 's': Key.S,
                 'w': Key.W,
             }
-            return switcher_chars.get(key_code.char, Key.UNKNOWN)
+            return switcher_chars.get(global_key.char, None)
 
         except AttributeError:
-            return Key.UNKNOWN
+            return None
 
